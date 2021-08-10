f24f222d62813d528062a407040e8cf08ed4a5b1
OpticRED <romhur87@kingshigh.school.nz>
2021-08-10 14:54:51 +1200

    Gmae
    OneGame

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,6 +2,24 @@ extends MarginContainer
 
 const FIRST_SCENE = "res://World1.tscn"
 
+func start_title_anim():
+	$AnimationPlayer.play("Title")
+	yield($AnimationPlayer,"animation_finished")
+	$AnimationPlayer.play("TitleJitter")
+
+func _ready():
+	start_title_anim()
+
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene(FIRST_SCENE)
+
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
+
+const Options = "res://Options.tscn"
+
+func _on_Options_pressed():
+	get_tree().change_scene(Options)
